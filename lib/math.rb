471200c419f4b0a7c7fea4a5384a8c71efcc7fda
Miguel Ben <itsmigben@gmail.com>
2019-12-05 17:11:33 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+  puts Math.addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  puts Math.subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  puts Math.division(num1, num2)
 end
 
 def multiplication(num1, num2)
-
+  puts Math.multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-
+  puts Math.modulo(num1, num2)
 end
 
 def square_root(num)
-
+  puts Math.square_root(num1, num2)
 end
